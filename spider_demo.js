1815ed8188b1bdf9b27d651f4cc29ef89e7202d6
Lionyu <779625444@qq.com>
2021-12-09 01:28:58 +0800

    update

--- spider_demo.js
+++ spider_demo.js
@@ -5,21 +5,37 @@ const cherrio = require("cheerio");
   const res = await axios.get("https://zhuanlan.zhihu.com/p/419468589");
   const html = res.data;
   const $ = cherrio.load(html);
-  const articleContent = $("article");
-  const doms = $(articleContent).find("header, .Post-RichTextContainer,img");
-  console.log(doms);
-  // const doms = $(articleContent).find("p, p>img, div, div>img");
+  const articleContent = $(".Post-NormalMain");
+  const dom = $(articleContent);
+  const content = getTextOrImg(dom, []);
+  // doms.map((i, d) => {
+  //   const text = $(d).text();
+  //   if (text) {
+  //     content.push(text);
+  //   } else if (d.name === "img") {
+  //     const src = $(d).attr("src");
+  //     content.push(src);
+  //   }
+  // });
 
-  const content = [];
-  doms.map((i, d) => {
-    const text = $(d).text();
-    if (text) {
-      content.push(text);
-    } else if (d.name === "img") {
-      const src = $(d).attr("src");
-      content.push(src);
+  function getTextOrImg(dom, arr) {
+    const d = $(dom);
+    const children = d.children();
+    if (children.length === 0) {
+      if (d.text()) {
+        arr.push(d.text());
+      }
+      if (d["0"].name === "img") {
+        arr.push(d.attr("src"));
+      }
+    } else {
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        getTextOrImg(child, arr);
+      }
     }
-  });
+    return arr;
+  }
 
   console.log(content);
 })()
